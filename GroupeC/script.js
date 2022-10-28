7a7db1066b725aea2b0f4c6ca7cbb52fb657e493
Jules Giraud <jules.giraud@outlook.fr>
2022-10-28 11:44:15 +0200

    prankex

--- GroupeC/script.js
+++ GroupeC/script.js
@@ -2,4 +2,5 @@ let p1 = document.querySelector(".p1");
 
 p1.addEventListener('mouseover', (e) => {
     console.log(e)
+    p1.classList.add("rotate");
 })
--- groupeB/script.js
+++ groupeB/script.js
@@ -2,4 +2,5 @@ let p1 = document.getElementById("p1");
 
 p1.addEventListener('mouseover', (e) => {
     console.log("Bonjour");
+    p1.classList.add("rotate");
 });

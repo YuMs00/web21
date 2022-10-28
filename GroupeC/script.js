60053f36f4cc355257f9ae843813e1ec4e162b93
Nathan MACAIGNE <nathan.macaigne@hotmail.com>
2022-10-28 11:57:25 +0200

    js

--- GroupeC/script.js
+++ GroupeC/script.js
@@ -3,3 +3,15 @@ let p1 = document.getElementById("p1");
 p1.addEventListener('mouseover', (event) => {
     console.log(event)
 });
+
+let p2 = document.getElementById("p1");
+
+p1.addEventListener('mouseover', (event) => {
+    console.log(event)
+});
+
+let p3 = document.getElementById("p1");
+
+p1.addEventListener('mouseover', (event) => {
+    console.log(event)
+});
